f4d1ba3b64786f9d2cf8b914c09ec7df7a0ee12b
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2019-11-14 13:05:50 +0530

    Added custom button components

---
+++ src/responseCustomizer/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const buttonStyle = {
+  backgroundColor: '#408cec',
+  border: 0,
+  padding: '12px 20px',
+  color: '#fff',
+  margin: 10,
+  borderRadius: 3,
+};
+
+
+const Button = ({ onModalResponseButtonClick, text }) => (
+  <button
+    type="button"
+    style={{ ...buttonStyle }}
+    onClick={onModalResponseButtonClick}
+    data-response={text}>
+    {text}
+  </button>
+);
+export default Button;
